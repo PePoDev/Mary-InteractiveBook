3f281ae27f51b6d698903d75be69f8eac2881b8d
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2020-03-24 19:45:32 +0700

    Part 1

---
+++ Assets/Scripts/ColorFade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorFade : MonoBehaviour
+{
+	private IEnumerator Start()
+	{
+		var image = GetComponent<Image>();
+
+		while (true)
+		{
+			image.DOFade(0f, .5f);
+			yield return new WaitForSeconds(.5f);
+			image.DOFade(1f, .5f);
+			yield return new WaitForSeconds(1f);
+		}
+	}
+}
--- Assets/Scripts/p10.cs
+++ Assets/Scripts/p10.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UIElements;
-using Image = UnityEngine.UI.Image;
+using UnityEngine.UI;
 
 public class p10 : MonoBehaviour
 {
@@ -16,11 +13,23 @@ public class p10 : MonoBehaviour
 	public Image สาย;
 	public Sprite ถอดสาย;
 
+	public Image telephone;
+
 	public Image notebook;
 	public Image notebookClose;
 	
-	private bool m_shakeState;
+	private bool m_shakeState = true;
+	private bool m_ringState = true;
 
+	public Button decline;
+	public Button cut;
+	public Button close;
+	
+	public Image moodlevel;
+	public Sprite[] levelSprite;
+
+	public GameObject next;
+	
 	private IEnumerator Start()
 	{
 		// Show tutorial
@@ -31,22 +40,74 @@ public class p10 : MonoBehaviour
 		Destroy(tutorial.gameObject);
 
 		// Shake Smart Phone
+		var i = 0;
 		while (m_shakeState)
 		{
-			smartPhone.GetComponent<RectTransform>().DOAnchorPosX(20, 1f);
-			yield return new WaitForSeconds(1f);
-			smartPhone.GetComponent<RectTransform>().DOAnchorPosX(-20, 1f);
-			yield return new WaitForSeconds(1f);
+			smartPhone.GetComponent<RectTransform>().DOAnchorPosX(30, .6f);
+			yield return new WaitForSeconds(.6f);
+			smartPhone.GetComponent<RectTransform>().DOAnchorPosX(-30, .6f);
+			yield return new WaitForSeconds(.6f);
+			
+			i++;
+			if (i > 2)
+			{
+				decline.gameObject.SetActive(true);
+			}
+			
+			if (!m_shakeState)
+			{
+				decline.gameObject.SetActive(false);
+			}
 		}
+		
+		// Rotate Telephone
+		i = 0;
+		while (m_ringState)
+		{
+			telephone.GetComponent<RectTransform>().DORotate(new Vector3(0,0,20), .6f);
+			yield return new WaitForSeconds(.6f);
+			telephone.GetComponent<RectTransform>().DORotate(new Vector3(0,0,-20), .6f);
+			yield return new WaitForSeconds(.6f);
+			
+			i++;
+			if (i > 2)
+			{
+				cut.gameObject.SetActive(true);
+			}
+
+			if (!m_ringState)
+			{
+				cut.gameObject.SetActive(false);
+			}
+		}
+		
+		yield return new WaitForSeconds(1f);
+		close.gameObject.SetActive(true);
 	}
 
 	public void Decline()
 	{
 		m_shakeState = false;
+		smartPhone.overrideSprite = smartPhoneOff;
+		decline.gameObject.SetActive(false);
+		moodlevel.overrideSprite = levelSprite[1];
 	}
 
-	private void Update()
+	public void Cut()
 	{
-		var x = GetComponent<Image>();
+		m_ringState = false;
+		สาย.overrideSprite = ถอดสาย;
+		cut.gameObject.SetActive(false);
+		moodlevel.overrideSprite = levelSprite[1];
+	}
+	
+	public void Close()
+	{
+		notebook.gameObject.SetActive(false);
+		notebookClose.gameObject.SetActive(true);
+		close.gameObject.SetActive(false);
+		moodlevel.overrideSprite = levelSprite[2];
+		
+		next.SetActive(true);
 	}
 }
